29d9bbbe6d0bfa0cde94c98cee8d172f58e2b316
dfamiliarstranger <eniolaemmanuel@gmail.com>
2025-01-06 15:44:27 +0100

    added render url

--- backend/settings.py
+++ backend/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'TRUE'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'demo-e-commerce-pwgp.onrender.com']
 
 
 # Application definition
